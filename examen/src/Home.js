8ca90993b801d1527cd1ea0fc1bbf4a3a4407980
Iliana <iliana.pinto04@gmail.com>
2019-02-17 19:28:58 -0600

    modificacion

--- examen/src/Home.js
+++ examen/src/Home.js
@@ -3,7 +3,7 @@ import {base} from './base.json';
 import Carousel from './Carousel';
 import Carouselp from './Carousep';
 import './App.css';
-
+import producto from './producto.jpeg'
 
 class Home extends Component{
     constructor(props){
@@ -16,6 +16,7 @@ class Home extends Component{
     return ( 
       <div>   
         <Carouselp></Carouselp>  
+        <img src={producto} width="1300x"></img>
         <nav><h1>NEW ARRIVALS</h1></nav>
         <Carousel></Carousel>
         
