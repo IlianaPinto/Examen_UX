2a767a1768475bf2e5a54a3bea4aea84edfcce0d
Iliana <iliana.pinto04@gmail.com>
2019-02-17 20:57:22 -0600

    search bar terminado

--- examen/src/Search.js
+++ examen/src/Search.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {base} from './base.json';
 import { Card, Label, Button, Image } from 'semantic-ui-react'
-import Formato from './Formato'
+import Cuadro from './Cuadro'
 
 function searchingfor(buscar){
     return function(x){
@@ -29,40 +29,16 @@ class Search extends Component{
         <div>
             <form>
                 <div className="component">
-                <br></br>
-                <input type="text" placeholder="Search" aria-label="Search" type="text" onChange={this.searchHandler} value={buscar} name='buscar'/>
-                
+                    <br></br>
+                    <input type="text" placeholder="Search" aria-label="Search" type="text" onChange={this.searchHandler} value={buscar} name='buscar'/>
                 </div>
                     
             </form>
             {base.filter(searchingfor(buscar)).map(base =>   
                 <div key={base.id}>
                     <div className="container">
-                    <div className="col-md-4">
-                        <div className="row mt-4">
-                            <div className = "card mt-4">
-                                <div className="card-body">        
-                                    <img src={base.imglink}  width="200px" height="200px"/>
-                                    <Card.Content>
-                                    <Card.Header style={{fontSize: 15}}>{base.tipo}</Card.Header>
-                                    <Card.Meta>
-                                        <Formato number={base.precio}/>
-                                    </Card.Meta>
-                                    <Card.Description>
-                                        <br></br>
-                                        <Label>{base.marca}</Label>
-                                        <Label>{base.status} in stock</Label>
-                                    </Card.Description>
-                                    </Card.Content>
-                                    <br></br>
-                                    <Card.Content extra>
-                                    <Button basic compact color='blue' floated='middle' onClick={this.onSaveProduct} > Add to car </Button>
-                                    </Card.Content>       
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div>          
+                        <Cuadro precio={base.precio} tipo={base.tipo} marca={base.marca} imglink={base.imglink}></Cuadro>        
+                    </div>        
                 </div>
             )
             }    
